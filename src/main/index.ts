c668bbfa657b2e0f485640de20f2b4d0e04b00ad
Matthew Blode <m@blode.co>
2023-11-25 22:59:42 +1100

    Save

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,10 @@
 import { processImage } from "./dither-image";
 import { createImageFromDistances } from "./distances-image";
 import { getNormalizedDistances } from "./normalize";
-import { generateTorusDiskPoints } from "./disk-points";
 import { createImageFromPoints } from "./points-image";
 import PoissonDiskSampling from 'poisson-disk-sampling'
 
-const inputPath = '2.jpg'
+const inputPath = '4 copy.png'
 const imageBase = 'f'
 const gridWidth = 256; // Define the width of the sampling area
 const gridHeight = 256; // Define the height of the sampling area
@@ -17,32 +16,13 @@ var p = new PoissonDiskSampling({
   minDistance: 2,
   maxDistance: 4,
   tries: 50,
-  // distanceFunction: function (point) {
-  //   // Custom distance function for the flat torus
-  //   // Implement the toroidal projection here
-  //   // For example, you can use modulo to wrap points around the torus
-  //   const x = (point[0] % gridWidth + gridWidth) % gridWidth;
-  //   const y = (point[1] % gridHeight + gridHeight) % gridHeight;
-  //   return Math.sqrt(x * x + y * y) / gridWidth; // Adjust as needed
-  // },
 });
 
 const points = p.fill();
-// const points = generateTorusDiskPoints({ gridWidth, gridHeight, n });
 
 const distances = getNormalizedDistances({ gridWidth, gridHeight, points, gamma });
 createImageFromPoints({ points, gridWidth, gridHeight, imageBase })
 createImageFromDistances({ distances, gridWidth, gridHeight, imageBase });
 processImage({ inputPath, distances, gridWidth, gridHeight, imageBase });
-
-
-// for (let i = 1; i < 10; i++) {
-//   const distances = getNormalizedDistances({ gridWidth, gridHeight, points, gamma: i * 0.15 });
-//   // createImageFromPoints({ points, gridWidth, gridHeight, imageBase })
-//   // createImageFromDistances({ distances, gridWidth, gridHeight, imageBase });
-//   processImage({ inputPath, distances, gridWidth, gridHeight, imageBase: `${imageBase}-${i}` });
-// }
-
-
 
 
